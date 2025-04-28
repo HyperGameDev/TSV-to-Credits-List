25a8a9d47a0755a6922b51cac60fa4be2a269aaa
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-04-28 16:25:11 -0500

    bit o renaming

--- addons/tsv_to_credits_list/tsv_credits_list.gd
+++ addons/tsv_to_credits_list/tsv_credits_list.gd
@@ -1,38 +1,38 @@
 @tool
 class_name CreditsList_TSV_Import extends CanvasLayer
 
-signal open_message_box
-signal close_message_box
+signal open_credits_list
+signal close_credits_list
 
-@onready var credits_container: MarginContainer = %MarginContainer_Messages
+@onready var credits_container: MarginContainer = %MarginContainer_Credits
 
 @onready var button_next: Button = %Button_Next
 
 var current_credit_number: int = 0
 
 
-var message_array: Array[String] = []
+var credits_array: Array[String] = []
 
 func _ready() -> void:		
 	button_next.pressed.connect(_on_next_pressed)
-	open_message_box.connect(_on_open_message_box)
-	close_message_box.connect(_on_close_message_box)
+	open_credits_list.connect(_on_open_credits_list)
+	close_credits_list.connect(_on_close_credits_list)
 	
 	if credits_container.get_children():
 		credits_container.get_child(0).visible = true
 	
 func _on_next_pressed() -> void:
 	if current_credit_number + 1 >= credits_container.get_children().size():
-		close_message_box.emit()
+		close_credits_list.emit()
 	else:
 		credits_container.get_child(current_credit_number).visible = false
 		current_credit_number += 1
 		credits_container.get_child(current_credit_number).visible = true
 	
-func _on_open_message_box() -> void:
+func _on_open_credits_list() -> void:
 	self.visible = true
 	
-func _on_close_message_box() -> void:
+func _on_close_credits_list() -> void:
 	self.visible = false
 	credits_container.get_child(current_credit_number).visible = false
 	current_credit_number = 0
